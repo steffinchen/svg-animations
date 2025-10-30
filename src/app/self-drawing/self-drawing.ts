ffa4d438c44cdbe57cf894135da0d1a99ab3f50e
Stefanie Erne <stefanie.erne@nxt.engineering>
2025-10-30 11:25:22 +0100

    dots moving around

--- src/app/self-drawing/self-drawing.ts
+++ src/app/self-drawing/self-drawing.ts
@@ -1,15 +1,23 @@
 import { Component, signal, ViewChild } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-self-drawing',
-  imports: [],
+    imports: [
+        NgClass
+    ],
   templateUrl: './self-drawing.html',
   styleUrl: './self-drawing.css',
 })
 export class SelfDrawing {
-  isVisible = signal(false);
+  isVisible = signal(true);
+  doCasinoLights = signal(false);
 
   draw(): void {
     this.isVisible.update((visible) => !visible);
+  }
+
+  toggleCasinoLights(): void {
+    this.doCasinoLights.update((cl) => !cl);
   }
 }
