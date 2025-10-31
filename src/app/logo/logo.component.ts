87b2a55a7de6ff705df2ad10e5b0b7a17b7f0a30
Stefanie Erne <stefanie.erne@nxt.engineering>
2025-10-31 17:59:08 +0100

    💫 morph the logo

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import gsap from 'gsap';
+import MorphSVGPlugin from 'gsap/MorphSVGPlugin';
 
 @Component({
   selector: 'app-logo',
@@ -6,4 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.css',
 })
-export class Logo {}
+export class Logo {
+  morphFrom = '#t1';
+  morphTo = '#bird1';
+
+  constructor() {
+    gsap.registerPlugin(MorphSVGPlugin);
+  }
+
+  trigger() {
+    gsap.to(this.morphFrom, { duration: 1, morphSVG: this.morphTo, type: 'rotational' });
+  }
+}
